17f815b76de91e3b70c348857b91745b962c116d
AmirAbasi <amir.abasi.dev@gmail.com>
2022-05-12 12:07:56 +0430

    feat : ✨ create todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Template from "./components/layout/Template";
 import AddTodo from "./components/todos/AddTodo";
 
 const App = () => {
+  const [todos, setTodos] = useState([]) 
+
+  const addTodoHandler = (todoName) => {
+    const newTodos = [...todos]
+    newTodos.push({todoName,isComplate: false})
+    setTodos(newTodos)
+  }
+
   return (
     <Template>
-      <AddTodo />
+      <AddTodo addTodoHandler={addTodoHandler}/>
     </Template>
   )
 }
--- src/components/todos/AddTodo.jsx
+++ src/components/todos/AddTodo.jsx
@@ -1,23 +1,25 @@
 import { useRef } from "react";
 
-const AddTodo = () => {
+const AddTodo = ({ addTodoHandler }) => {
   const todoName__Ref = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    const todoName = todoName__Ref.current.value
-
-    //Add Todo Function
-
-    console.log(todoName)
-    todoName__Ref.current.value = '';
-  }
+    const todoName = todoName__Ref.current.value;
+    addTodoHandler(todoName);
+    todoName__Ref.current.value = "";
+  };
 
   return (
     <form className="row" onSubmit={submitHandler}>
       <div className="col">
-        <input className="form-control" placeholder="Enter Todo Name" type="text" ref={todoName__Ref} />
+        <input
+          className="form-control"
+          placeholder="Enter Todo Name"
+          type="text"
+          ref={todoName__Ref}
+        />
       </div>
       <div className="col-2">
         <button className="btn btn-primary w-100">Add Todo</button>
